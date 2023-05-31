d760a9b4f7aea8d1173abe82a2c1bbe70fcd9446
tudor <114509299+tudoorr@users.noreply.github.com>
2023-05-31 22:33:14 +0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket, threading
+from string import punctuation
+
+host = "IP_HERE"
+port = PORT_HERE
+
+username = input("enter username: ")
+
+while any(char in punctuation for char in username) or len(username) > 10 or len(username) < 2 or " " in username or "THISISAUSERNAME" in username:
+    print("\ninvalid username\n")
+    username = input("enter username: ")
+
+username += " THISISAUSERNAME"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+client.send(username.encode())
+
+def send():
+    while True:
+        client.send(input("").encode())
+
+def listen():
+    while True:
+        try:
+            print(client.recv(1024).decode())
+        except:
+            print("\nerror!!!!!!!!!!!!!!!\n")
+            client.shutdown(socket.SHUT_RDWR)
+            exit()
+
+threading.Thread(target=listen).start()
+threading.Thread(target=send).start()
